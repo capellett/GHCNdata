21cfa3379f06e4db300eb086993485d2a96ab68c
alex <c.alex.pellett@gmail.com>
2018-12-20 21:17:26 -0500

    doc

---
+++ R/GHCNdata-package.R
@@ -0,0 +1,9 @@
+#' Some Global Historical Climatological Network Data
+#'
+#' An R package with some data from the Global Historical Climatological Network.
+#'     Also includes several functions for dealing with that data.
+#'
+#' @docType package
+#' @name GHCNdata
+#'
+NULL
